718ceda80a37f7153ad39d6a3bb661eb7269ede3
Lester Trejos <lestertb@estudiantec.cr>
2022-04-15 14:10:17 -0600

    Cambios

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,9 @@ from joblib import load
 # Import apifast to create api rest
 from fastapi import Request, FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
+import sklearn
+from array import array
+import numpy as np
 
 # Create simple api rest
 app = FastAPI()
@@ -34,4 +37,9 @@ async def root(id: int):
 # Post body request
 @app.post("/model1")
 async def login(input1: str = Form(...), input2: str = Form(...)):
-    return {"input1": input1, "input2": input2}
+    # Load model
+    model = load("Models\modelo_Autos")
+    result = model.predict(np.array([[-1.82411162, -0.56392439, -0.8299164, -0.5782049,  0.50018321,
+                                      1.35632689,  0.39148015, -0.17450057]]))
+    print(result)
+    return {"result": "result"}
